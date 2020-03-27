7533a3847e6f2bc9e964e1b30b771dfcb285e73e
shiyuan <shiyuan@nao.cas.cn>
2020-03-27 19:36:43 +0800

    first_commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools  import setup
+from setuptools import find_packages
+
+setup(
+    name= 'dslpy',
+    version= '0.0.1',
+    packages=find_packages(),
+    author='Yuan Shi',
+    author_email=' ',
+    url = " "
+)
